dd5754edc85e76eea7950bfbc63fe54dea1fb5e2
Stephen Stubbs <stephen.stubbs@vanqor.com>
2020-01-19 04:06:01 +0400

    change default release name to pgxlo

--- src/vars.rs
+++ src/vars.rs
@@ -2,7 +2,7 @@ pub static NAMESPACE: &str = "pgxl";
 pub static CUSTOM_RESOURCE_GROUP: &str = "postgres-xl-operator.vanqor.com";
 pub static CHART_NAME: &str = "postgres-xl-operator-chart";
 pub static CHART_VERSION: &str = "0.0.1";
-pub static RELEASE_NAME: &str = "postgres-xl-operator";
+pub static RELEASE_NAME: &str = "pgxlo";
 pub static RELEASE_SERVICE: &str = "helm";
 pub static RUST_LOG: &str = "info,kube=trace";
 //pub static CLUSTER_RESOURCE_SINGULAR: &str = "postgres-xl-cluster";

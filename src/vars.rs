cc20baf9f9282df517622222fb52d35606f52621
Stephen Stubbs <stephen.stubbs@vanqor.com>
2020-01-22 05:47:27 +0400

    add separate client config for incluster.

--- src/vars.rs
+++ src/vars.rs
@@ -4,7 +4,7 @@ pub static CHART_NAME: &str = "postgres-xl-operator-chart";
 pub static CHART_VERSION: &str = "0.1.0";
 pub static RELEASE_NAME: &str = "pgxlo";
 pub static RELEASE_SERVICE: &str = "helm";
-pub static LOG_LEVEL: &str = "info,kube=trace";
+pub static LOG_LEVEL: &str = "info";
 pub static KUBE_CONFIG_TYPE: &str = "incluster";
 //pub static CLUSTER_RESOURCE_SINGULAR: &str = "postgres-xl-cluster";
 pub static CLUSTER_RESOURCE_PLURAL: &str = "postgres-xl-clusters";
